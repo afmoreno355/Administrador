326655acd33be4b217798e344ea62abfff9e5dbb
Cristian Avella H <cfavella@sena.edu.co>
2023-05-23 17:49:51 -0500

    Update clase contratos

--- View/Contratos/ContratosTabla.php
+++ View/Contratos/ContratosTabla.php
@@ -35,20 +35,20 @@ if ($ingreso === false && $permisos->getIdTipo() !== "SA") {
 
     // evalua si existe bucarPalabraClave y nos crea la cadena de busqueda
     if ($bucarPalabraClave != "") {
-        $filtro .= " ( codigocargo like '%" . strtoupper($bucarPalabraClave) . "%' or  nombrecargo like '%" . strtoupper($bucarPalabraClave) . "%' )";
+        $filtro .= " ( codigocontratos like '%" . strtoupper($bucarPalabraClave) . "%' or  nombrecontratos like '%" . strtoupper($bucarPalabraClave) . "%' )";
     }
 
     // obj para llenar las tablas
-    $contratos = Contratos::datosobjetos($filtro, $pagina, 20);
+    $cargo = Cargo::datosobjetos($filtro, $pagina, 20);
     // numero de paginas para la paginacion
-    $numeroPaginas = ceil(Contratos::count($filtro)[0][0] / 20);
+    $numeroPaginas = ceil(Cargo::count($filtro)[0][0] / 20);
     // ecrypt codifica lo que enviamos por javascript   
     $var_add = Http::encryptIt("id=1&llave_Primaria=&user={$_SESSION["user"]}&accion=ADICIONAR");
     $var_ayu = Http::encryptIt("id=4&llave_Primaria=&user={$_SESSION["user"]}&accion=AYUDA");
     
     ?> 
     <div class="botonMenu" style="font-weight: bolder; font-size: 2em; ">
-        <button type='button' id='button' class="ele" title='Adicionar nuevo'  onclick="validarDatos(``, `I=<?= $var_add ?>`, `modalVentana`, `View/Cargo/CargoModales.php`, event, 'ele')"><img src="img/icon/adds.png"/> ADICIONAR<br>CONTRATO</button>
+        <button type='button' id='button' class="ele" title='Adicionar nuevo'  onclick="validarDatos(``, `I=<?= $var_add ?>`, `modalVentana`, `View/Cargo/CargoModales.php`, event, 'ele')"><img src="img/icon/adds.png"/> ADICIONAR<br>CARGO</button>
         <button type='button' id='button' class="ele" title='Ayuda'  onclick="validarDatos(``, `I=<?= $var_ayu ?>`, `modalVentana`, `View/Cargo/CargoModales.php`, event, 'ele')"><img src="img/icon/ayu.png"/> AYUDA<br>MODULO</button>
     </div>
     <!-- Inicio de html tablas -->
@@ -61,8 +61,8 @@ if ($ingreso === false && $permisos->getIdTipo() !== "SA") {
             <th>ACCION</th>        
         </tr>
         <?PHP
-        for ($i = 0; $i < count($contratos); $i++) {
-            $objet = $contratos[$i];
+        for ($i = 0; $i < count($cargo); $i++) {
+            $objet = $cargo[$i];
             $var_mod = Http::encryptIt("id=1&llave_Primaria={$objet->getId()}&user={$_SESSION["user"]}&accion=MODIFICAR");
             $var_eli = Http::encryptIt("id=2&llave_Primaria={$objet->getId()}&user={$_SESSION["user"]}&accion=ELIMINAR");
             $var_blo = Http::encryptIt("id=5&llave_Primaria={$objet->getId()}&user={$_SESSION["user"]}&accion=BLOQUEAR");
@@ -74,18 +74,18 @@ if ($ingreso === false && $permisos->getIdTipo() !== "SA") {
                 <td> <?= $objet->getNombrecargo() ?></td>
                 <td> <?= $objet->getDetalle() ?></td>
                 <td>
-                    <input type="button" id="button" name="1" onclick="validarDatos(``, `I=<?= $var_inf ?>`, `modalVentana`, `View/Contratos/CargoModales.php`)" title="Información Elemento" value="INFORMACION">
-                    <input type="button" id="button" name="1" onclick="validarDatos(``, `I=<?= $var_blo ?>`, `modalVentana`, `View/Contratos/CargoModales.php`)" title="Bloquear Elemento" value="BLOQUEAR">
+                    <input type="button" id="button" name="1" onclick="validarDatos(``, `I=<?= $var_inf ?>`, `modalVentana`, `View/Cargo/CargoModales.php`)" title="Información Elemento" value="INFORMACION">
+                    <input type="button" id="button" name="1" onclick="validarDatos(``, `I=<?= $var_blo ?>`, `modalVentana`, `View/Cargo/CargoModales.php`)" title="Bloquear Elemento" value="BLOQUEAR">
                 </td>
                 <td>
-                    <input type="button" id="button" name="3" onclick="validarDatos(``, `I=<?= $var_mod ?>`, `modalVentana`, `View/Contratos/CargoModales.php`)" title="Modificar Elemento" value="MODIFICAR">
-                    <input type="button" id="button" name="3" onclick="validarDatos(``, `I=<?= $var_eli ?>`, `modalVentana`, `View/Contratos/CargoModales.php`)" title="Eliminar" value="ELIMINAR">
+                    <input type="button" id="button" name="3" onclick="validarDatos(``, `I=<?= $var_mod ?>`, `modalVentana`, `View/Cargo/CargoModales.php`)" title="Modificar Elemento" value="MODIFICAR">
+                    <input type="button" id="button" name="3" onclick="validarDatos(``, `I=<?= $var_eli ?>`, `modalVentana`, `View/Cargo/CargoModales.php`)" title="Eliminar" value="ELIMINAR">
                 </td>
             </tr>
         <?PHP
     }
     ?>
-        <input type="hidden" id="donde" value="Contratos">
+        <input type="hidden" id="donde" value="Cargo">
         <input type="hidden" id="id_espe" value="">
         <input type="hidden" id="numeroPaginas" value="<?= $numeroPaginas ?>">
         <input type="hidden" id="sedeGestion" value="">
